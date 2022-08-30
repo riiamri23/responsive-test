8ccc329b83c465dd9d6ab64d6ea280cc358c629a
riiamri <syaeful.amri@dataon.com>
2022-08-30 22:08:39 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,163 @@
+<script setup>
+import backgroundPath from "./assets/imgs/background2.webp";
+</script>
+
+<template>
+  <div
+    class="grid place-items-center w-screen min-h-screen"
+    :style="{
+      backgroundImage: `url(${backgroundPath})`,
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover',
+    }"
+  >
+    <div class="mx-auto grid place-items-center">
+      <h1 class="text-[#ffffff] text-3xl mb-2">Responsive Check</h1>
+      <div class="flex">
+        <button class="self-start" @click="onResponsiveChange(1)">
+          <DesktopSvg class="w-8 fill-white" />
+
+          <div
+            class="mt-1 mx-auto w-1/2 bg-[#ffffff] h-1 rounded-sm"
+            :class="{ hidden: button.active != 1 }"
+          ></div>
+        </button>
+        <button class="self-start" @click="onResponsiveChange(2)">
+          <TabletSvg class="w-8 fill-white" />
+
+          <div
+            class="mt-1 mx-auto w-1/2 bg-[#ffffff] h-1 rounded-sm"
+            :class="{ hidden: button.active != 2 }"
+          ></div>
+        </button>
+        <button class="self-start" @click="onResponsiveChange(3)">
+          <SmartphoneSvg class="w-8 fill-white" />
+
+          <div
+            class="mt-1 mx-auto w-1/2 bg-[#ffffff] h-1 rounded-sm"
+            :class="{ hidden: button.active != 3 }"
+          ></div>
+        </button>
+        <button id="dropdownDefault" data-dropdown-toggle="dropdown" class="selft-start">
+          <MoreSvg class="w-8 fill-white" />
+        </button>
+        <!-- Dropdown menu
+        <div id="dropdown" class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700">
+            <ul class="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefault">
+              <li>
+                <a href="#" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
+              </li>
+              <li>
+                <a href="#" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
+              </li>
+              <li>
+                <a href="#" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
+              </li>
+              <li>
+                <a href="#" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>
+              </li>
+            </ul>
+        </div> -->
+      </div>
+      <div
+        class="flex m-4 transition-all duration-1000 hover:w-full"
+        id="iframe-container"
+      >
+        <div class="w-2 h-10 bg-[#b3b3b3] rounded-xl my-auto m-4"></div>
+        <iframe
+          class=""
+          width="100%"
+          height="100%"
+          src="https://play.tailwindcss.com/suMPwQ7jBY?layout=preview"
+          frameborder="0"
+          webkitallowfullscreen
+          mozallowfullscreen
+          allowfullscreen
+        ></iframe>
+        <div class="w-2 h-10 bg-[#b3b3b3] rounded-xl my-auto m-4"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+#iframe-container {
+  width: 1920px;
+  height: 1080px;
+}
+</style>
+
+<script>
+import DesktopSvg from "./svgs/desktopsvg.vue";
+import TabletSvg from "./svgs/tabletsvg.vue";
+import SmartphoneSvg from "./svgs/smartphonesvg.vue";
+import MoreSvg from "./svgs/moresvg.vue";
+
+export default {
+  components: { DesktopSvg, TabletSvg, SmartphoneSvg, MoreSvg },
+  data() {
+    return {
+      iFrame: {
+        width: 1920,
+        height: 1080,
+      },
+      button: {
+        active: 1,
+      },
+      increment: 1,
+    };
+  },
+  methods: {
+    onResponsiveChange(selectedSize) {
+      /*
+       * list responsive page
+       * desktop 1024×768 through 1920×1080.
+       * tablet 601×962 through 1280×800.
+       * mobile 360×640 through 414×896.
+       */
+
+      if (selectedSize == 1) {
+        //desktop
+        this.iFrame.width = 1920;
+        this.iFrame.height = 1080;
+        this.button.active = 1;
+      } else if (selectedSize == 2) {
+        //tablet
+        this.iFrame.width = 1280;
+        this.iFrame.height = 800;
+        this.button.active = 2;
+      } else if (selectedSize == 3) {
+        //mobile
+        this.iFrame.width = 414;
+        this.iFrame.height = 896;
+        this.button.active = 3;
+      }
+
+      //animation
+      document.getElementById(
+        "iframe-container"
+      ).style.width = `${this.iFrame.width}px`;
+      document.getElementById(
+        "iframe-container"
+      ).style.height = `${this.iFrame.height}px`;
+    },
+  },
+  mounted() {
+    window.setInterval(() => {
+      if(this.button.active == 1){
+        this.increment = 1;
+      }
+
+      if(this.button.active == 3){
+        this.increment = -1;
+      }
+
+      this.button.active = this.button.active + this.increment;
+      console.log(this.button.active);
+      this.onResponsiveChange(this.button.active);
+    }, 2500);
+  },
+
+  beforeDestroy() {},
+};
+</script>
---
+++ src/svgs/moresvg.vue
@@ -0,0 +1,50 @@
+<template>
+  <svg
+    version="1.1"
+    id="Layer_1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    x="0px"
+    y="0px"
+    viewBox="0 0 472.576 472.576"
+    style="enable-background: new 0 0 472.576 472.576"
+    xml:space="preserve"
+  >
+    <g>
+      <g>
+        <circle cx="65.142" cy="236.288" r="65.142" />
+      </g>
+    </g>
+    <g>
+      <g>
+        <circle cx="236.308" cy="236.288" r="65.142" />
+      </g>
+    </g>
+    <g>
+      <g>
+        <circle cx="407.434" cy="236.288" r="65.142" />
+      </g>
+    </g>
+    <g></g>
+    <g></g>
+    <g></g>
+    <g></g>
+    <g></g>
+    <g></g>
+    <g></g>
+    <g></g>
+    <g></g>
+    <g></g>
+    <g></g>
+    <g></g>
+    <g></g>
+    <g></g>
+    <g></g>
+  </svg>
+</template>
+
+<script>
+export default{
+    name: 'MoreSvg',
+}
+</script>
